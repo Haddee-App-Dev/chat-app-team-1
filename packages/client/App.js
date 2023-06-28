b692e8bc6327609ba2adc72a359ffab3a1416f43
Aryan Ziyar <aryanziyar3@gmail.com>
2023-06-28 14:40:23 -0700

    setup

--- packages/client/App.js
+++ packages/client/App.js
@@ -1,10 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  let name = 'John'
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
+      <Text>Hello! ${name}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +14,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
