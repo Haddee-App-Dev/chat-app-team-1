e36eb5928744ca5408e0a86baf71d8e023fbeeb9
EricS <song_xinle@163.com>
2023-07-12 11:19:14 +0800

    [unfinished] basic layout of login screen

--- packages/client/App.js
+++ packages/client/App.js
@@ -1,29 +1,35 @@
 import { StatusBar } from "expo-status-bar";
 import * as React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { StyleSheet, Text, View } from "react-native";
 import { ChatScreen } from './components/chatscreen';
 import { ContactScreen } from './components/contactscreen';
 import { SelfProfileScreen } from "./components/profile";
+import { LoginScreen } from "./screens/Login";
 import { Icon } from 'react-native-elements';
 
 export default function App() {
   const Tab = createBottomTabNavigator();
+  const Stack = createStackNavigator();
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Chats" component={ChatScreen} options={{
-          tabBarIcon: () => (<Icon name="chat" />)
-        }} />
-        <Tab.Screen name="Contacts" component={ContactScreen} options={{
-          tabBarIcon: () => (<Icon name="person" />)
-        }} />
-        <Tab.Screen name="Me" component={SelfProfileScreen} options={{
-          headerShown: false,
-          tabBarIcon: () => (<Icon name="person" />)
-        }} />
-      </Tab.Navigator>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Tab.Navigator>
+          <Tab.Screen name="Chats" component={ChatScreen} options={{
+            tabBarIcon: () => (<Icon name="chat" />)
+          }} />
+          <Tab.Screen name="Contacts" component={ContactScreen} options={{
+            tabBarIcon: () => (<Icon name="person" />)
+          }} />
+          <Tab.Screen name="Me" component={SelfProfileScreen} options={{
+            headerShown: false,
+            tabBarIcon: () => (<Icon name="person" />)
+          }} />
+        </Tab.Navigator>
+      </Stack.Navigator>
     </NavigationContainer>
   );
 };
--- packages/client/screens/Login.js
+++ packages/client/screens/Login.js
@@ -1,10 +1,29 @@
-import React from "react";
-import { View, Button } from "react-native";
-const Login = () => {
+import React, { useState } from "react";
+import { View, TextInput, Button } from "react-native";
+const Login = ({ navigation }) => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin = () => {
+        //additional login logic required
+        navigation.navigate('Chats');
+    }
+
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignContent: 'center' }}>
-            <Text> Login </Text>
+            <TextInput
+                placeholder="Username"
+                value={username}
+                onChangeText={setUsername}
+            />
+            <TextInput
+                placeholder="Password"
+                secureTextEntry
+                value={password}
+                onChangeText={setPassword}
+            />
+            <Button title="Login" onPress={handleLogin} />
         </View>
-    )
-}
+    );
+};
 export default Login
